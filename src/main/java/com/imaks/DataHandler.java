8bc8d6931b95ff68f5b608c973514d1fe268897b
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-11-04 12:15:13 +0100

    Init commit

---
+++ src/main/java/com/imaks/DataHandler.java
@@ -0,0 +1,21 @@
+package com.imaks;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DataHandler {
+
+    String[] fruits = new DataRepository().getData();
+
+    public void getOutput() {
+        // критичний блок коду
+        synchronized (this) {
+            StringBuilder sb = new StringBuilder();
+            AtomicInteger count = new AtomicInteger(1);
+            for (String fruit : fruits) {
+                sb.append(String.format("(%d) %s ",
+                        count.getAndIncrement(), fruit));
+            }
+            System.out.println(Thread.currentThread().getName() + ": " + sb);
+        }
+    }
+}
